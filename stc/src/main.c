6a87f9e0cdedb71bdbfc3b25e84491bd1d6d4e35
Huacai Sang <sanghuacai@gmail.com>
2021-11-09 10:03:18 +0100

    update

--- stc/src/main.c
+++ stc/src/main.c
@@ -5,6 +5,8 @@ void task0(void);
 void task1(void);
 void (* const tasks[OS_TASKS])(void) = {task0, task1};
 
+const u8 HALF_DUTY = 127;
+
 void main(void)
 {
     enter_critical();
@@ -12,8 +14,7 @@ void main(void)
     gpio_mode(GPIO_OUTPUT_PUSH_PULL, 3, 0);
     gpio_mode(GPIO_OUTPUT_PUSH_PULL, 3, 1);
 
-    u8 duty = 127;
-    set_duty(0, duty);
+    set_duty(0, HALF_DUTY);
     set_tone(1, 440);
     start_pwm();
    
